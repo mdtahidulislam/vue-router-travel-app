8d308b19f606a4a278e5d161910eca399dc991af
mdtahidulislam <tahidul.developer@gmail.com>
2021-06-22 17:49:31 +0600

    vue-router-app

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/brazil">Brazil</router-link>
+    <router-link to="/jamaica">Jamaica</router-link>
+    <router-link to="/hawaii">Hawaii</router-link>
+    <router-link to="/panama">Panama</router-link>
+  </div>
+</template>
+
+<style scoped lang="css">
+    #nav .active-link {
+        color: #44BA81;
+        border-bottom: 1px solid #44BA81;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+// import Brazil from "../views/Brazil.vue";
+// import Hawaii from "../views/Hawaii.vue";
+// import Jamaica from "../views/Jamaica.vue";
+// import Panama from "../views/Panama.vue";
 
 const routes = [
   {
@@ -16,11 +20,36 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path: "/brazil",
+    name: "Brazil",
+    component: () => import(/* webpackChunkName: "Brazil" */ "../views/Brazil.vue"),
+  },
+  {
+    path: "/hawaii",
+    name: "Hawaii",
+    component: () => import(/* webpackChunkName: "Hawaii" */ "../views/Hawaii.vue"),
+  },
+  {
+    path: "/panama",
+    name: "Panama",
+    component: () => import(/* webpackChunkName: "Panama" */ "../views/Panama.vue"),
+  },
+  {
+    path: "/jamaica",
+    name: "Jamaica",
+    component: () => import(/* webpackChunkName: "Jamaica" */ "../views/Jamaica.vue"),
+  },
+  {
+    path: "/destination:id",
+    component: () => import("../views/DestinationShow.vue"),
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  linkActiveClass: "active-link",
 });
 
 export default router;
